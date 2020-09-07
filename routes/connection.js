817a2c3ba1323b3aaf8c08ad6120cab2eb6a8339
Thomas Coradetti <tmcoradetti@gmail.com>
2020-09-07 18:56:16 -0400

    fixing files

--- routes/connection.js
+++ routes/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: " ",
+    password: "F1gur30ut@bl3",
     database: "habits_db"
   });
 }
